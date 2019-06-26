a35a676b9f2701f3e9222000889a54a477c78790
dapplion <guillem.lion.benosa@gmail.com>
2019-06-26 21:40:50 +0200

    Auto-docs manifest reference generator

---
+++ generateManifestReference.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export MANIFEST_PATH=manifest-reference.json
+# Fetches the latest manifest in Github without downloading the rest of the package
+curl https://raw.githubusercontent.com/dappnode/dnp-manifest/master/src/manifest.schema.json > $MANIFEST_PATH
+# Run the schema to markdown tool customized for the DAppNode docs repo
+# -x - : supresses the output of the validated schema
+# : the .md is outputed at the default 
+npx git://github.com/dapplion/jsonschema2md.git -d $MANIFEST_PATH -x - -o docs
+# Clean manifest schema
+rm $MANIFEST_PATH
