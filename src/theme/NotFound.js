9778f5ffbdbe20aa3c30d655cfe62240feca28a0
Tropicar <zerozept@protonmail.com>
2021-06-15 17:57:06 +0200

    Add custom 404 page

---
+++ src/theme/NotFound.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+
+
+function NotFound() {
+    let currentLocation =  window.location.href;
+    return <Layout title="Page Not Found">
+        <main className="rds-404">  
+          <div className="container">
+            <div className="row">
+              <div className="col col--8 col--offset-2">
+                <h1>
+                  Oh oh... You probably weren't looking for this.
+                </h1>
+                <p>            
+                We've changed the documentation to serve you better and we might have broken this link {currentLocation} .
+                </p>
+                <p> 
+                If you got here following one of our links, please report the broken link. 
+                <a href="https://github.com/dappnode/DAppNodeDocs/issues/new/choose" className="link"> here</a>
+                </p>
+                <a href="/" className="link">Back to homepage</a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </Layout>;
+  }
+  
+  export default NotFound;
