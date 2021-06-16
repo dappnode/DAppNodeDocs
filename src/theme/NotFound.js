71138b584411879ee5e0cb6cdfd88880d98daacd
Tropicar <zerozept@protonmail.com>
2021-06-16 08:57:14 +0200

    Add style to 404 custom page

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -5,25 +5,19 @@ import Layout from '@theme/Layout';
 
 function NotFound() {
     return <Layout title="Page Not Found">
-         
-          <div className="container">
-            <div className="row">
-              <div className="col col--8 col--offset-2 ">
-                <h1>
-                  Oh oh... You probably weren't looking for this.
-                </h1>
-                <p>            
-                We've changed the documentation to serve you better and we might have broken this link. 
-                </p>
-                <p> 
-                If you got here following one of our links, please report the broken link
-                <a href="https://github.com/dappnode/DAppNodeDocs/issues/new/choose" className="link"> here</a>.
-                </p>
-                <a href="/" className="link">Back to homepage</a>
-              </div>
-            </div>
-          </div>
-       
+        <div className = "notFound">
+            <h1>
+            Oh oh... You probably weren't looking for this.
+            </h1>
+            <p>            
+            We've changed the documentation to serve you better and we might have broken this link. 
+            </p>
+            <p> 
+            If you got here following one of our links, please report the broken link
+            <a href="https://github.com/dappnode/DAppNodeDocs/issues/new/choose" className="link"> here</a>.
+            </p>
+            <a href="/" className="link">Back to homepage</a>
+        </div>
       </Layout>;
   }
   
