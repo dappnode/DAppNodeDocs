ef005cf125ec8109ab4c977d6a841bf622842c8f
Tropicar <zerozept@protonmail.com>
2021-06-18 09:50:57 +0200

    Remove the edit button
    This  button does not make sense

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,15 +88,11 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/dappnode/DAppNodeDocs',
             routeBasePath: '/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/dappnode/DAppNodeDocs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
