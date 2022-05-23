e9f5f73ac7a976f4940c9a69a139d96615d9aa31
Tropicar <zerozept@protonmail.com>
2022-05-23 13:59:43 +0200

    Add the user section in top bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
       items: [
         {to: '/', label: 'Docs', position: 'left'},
+        {to: '/user', label: 'User Guide', position: 'left'},
         {to: '/dao', label: 'DAO', position: 'left'},
         {
           type: 'localeDropdown',
