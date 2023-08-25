f021bb7d9112f7fa7932df91b9378fd4c32f6c68
Marketen <marcfont12@gmail.com>
2023-08-25 17:22:28 +0200

    fix deploy try

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/logo.png",
 
   // Set the production url of your site here
-  url: "https://docs.dappnode.io/",
+  url: 'https://docs.dappnode.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -41,6 +41,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/dappnode/DappnodeDocs",
