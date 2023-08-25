a9053636533b987e9771a210da1fc95b21c24e6d
Marc Font <36164126+Marketen@users.noreply.github.com>
2023-08-25 17:35:06 +0200

    fix deploy try (#290)
    * fix deploy try

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/logo.png",
 
   // Set the production url of your site here
-  url: "https://docs.dappnode.io/",
+  url: 'https://docs.dappnode.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
