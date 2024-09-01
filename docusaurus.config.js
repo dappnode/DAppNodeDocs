3a663b40b40d069ae9f2024bb4f2676a55ce5646
calumtomeny <939917+calumtomeny@users.noreply.github.com>
2024-09-01 23:39:02 +0100

    Update docusaurus.config.js
    fix(config): update edit url so that "Edit this page" links are prepended with "edit/master" so that they resolve to the appropriate edit page instead of returning 404.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/dappnode/DappnodeDocs",
+          editUrl: "https://github.com/dappnode/DappnodeDocs/edit/master",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
