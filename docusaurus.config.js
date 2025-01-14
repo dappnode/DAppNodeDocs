b352b5a79e24b5cb163877f08bb6d7cbd64c3b8c
pablomendezroyo <mendez4a@gmail.com>
2025-01-14 18:19:28 +0100

    fix redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,12 +37,18 @@ const config = {
     [
       "@docusaurus/plugin-client-redirects",
       {
+        fromExtensions: ["html", "htm"], // /myPage.html -> /myPage
+        toExtensions: [], // No specific file extensions
         redirects: [
+          // Redirect from old Lido page to the overview page
           {
-            from: "/docs/user/staking/ethereum/lsd-pools/lido/",
             to: "/docs/user/staking/ethereum/lsd-pools/lido/overview",
+            from: "/docs/user/staking/ethereum/lsd-pools/lido/",
           },
         ],
+        createRedirects(existingPath) {
+          return undefined; // No additional redirects created
+        },
       },
     ],
   ],
